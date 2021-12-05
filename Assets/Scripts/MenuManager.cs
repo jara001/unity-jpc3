7f396f0d2fcaeda4545819ff16f40e7813edbe25
Jaroslav Klapálek <jaroslavklapalek@seznam.cz>
2021-12-05 16:11:35 +0100

    Make start button working

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public static MenuManager instance;
+
+    public int bestScore;
+    public string bestScoreUser;
+
+    public string playerName;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
